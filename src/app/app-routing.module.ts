fb2dc6c6dc5109dbdd3c3baed8424d647b9ef3ff
Anna Wagner <annwagner8888@gmail.com>
2022-06-08 19:32:55 +0300

    HP-20: add skeleton

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ContentLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
